cf0eab4ab1dc091e659585ff644d53a365ae1a6e
pavelp21 <pavelvp2182@yandex.ru>
2017-07-27 13:44:39 +0500

    Верстка 4го экрана

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+        $("a.text-center").click(function(event) {
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({
+                scrollTop: top
+            }, 500);
+        });
+    });
