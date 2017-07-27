5da3bbaf726b6bfbb04cbeb1dcc5046a9512be2b
pavelp21 <pavelvp2182@yandex.ru>
2017-07-27 19:58:16 +0500

    Завершение верстки 1.0

---
+++ mail/sendmail.php
@@ -0,0 +1,24 @@
+<?php 
+
+
+
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Спасибо за заявку!</title>
+ 	<link rel="stylesheet" href="../css/bootstrap.min.css">
+ 	<link rel="stylesheet" href="../css/styles.css">
+ </head>
+ <body>
+ 	<section class="first_screen">
+ 		<div class="container">
+ 			<div class="jumbotron">
+ 				<h1>Подтвердите Ваше участие на почте</h1>
+ 			</div>
+ 		</div>
+ 	</section>
+ </body>
+ </html>
