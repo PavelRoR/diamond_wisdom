4469ed5d26bbd39eae557f26dcab8379e43227b1
pavelp21 <pavelvp2182@yandex.ru>
2017-09-01 21:58:22 +0500

    Правки респонсива

--- dengi/js/scripts.js
+++ dengi/js/scripts.js
@@ -1,1 +1,15 @@
-$(document).ready(function () {$("a.text-center").click(function (event) {event.preventDefault();var id = $(this).attr('href'),top = $(id).offset().top;$('body,html').animate({scrollTop: top}, 500);});$(".js-video").click(function () {var dataYoutube = $(this).attr('data-youtube');$(this).html('<iframe src="https://www.youtube.com/embed/' + dataYoutube + '?showinfo=0&autoplay=1" frameborder="0" class="img-thumbnail" allowfullscreen></iframe>')});});
+$(document).ready(function () {
+	$("a.text-center").click(function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({
+			scrollTop: top
+		}, 500);
+	});
+	$(".js-video").click(function () {
+		var dataYoutube = $(this).attr('data-youtube');
+		$(this).html('<iframe src="https://www.youtube.com/embed/' + dataYoutube + '?showinfo=0&autoplay=1" frameborder="0" class="img-thumbnail" allowfullscreen></iframe>')
+	});
+
+});
